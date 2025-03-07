3f3243dfa159b50264c4377641afd6957c1f1f4d
Shad0w <kejkej120@gmail.com>
2025-03-07 14:23:23 +0100

    fix

--- firebase_config.py
+++ firebase_config.py
@@ -4,13 +4,20 @@ import firebase_admin
 from firebase_admin import credentials
 from dotenv import load_dotenv
 
-# Wczytaj zmienne środowiskowe z pliku .env
+# Wczytaj zmienne środowiskowe
 load_dotenv()
 
-# Pobierz poświadczenia z zmiennej środowiskowej
-firebase_credentials = os.getenv('FIREBASE_CREDENTIALS')
-credentials_dict = json.loads(firebase_credentials)
+# Pobierz poświadczenia z env
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
 
-# Zainicjalizuj Firebase za pomocą tych poświadczeń
-cred = credentials.Certificate(credentials_dict)
-firebase_admin.initialize_app(cred)
+# Jeśli nie jest None, sparsuj JSON
+if firebase_credentials:
+    try:
+        credentials_dict = json.loads(firebase_credentials)
+        cred = credentials.Certificate(credentials_dict)
+        firebase_admin.initialize_app(cred)
+        print("✅ Firebase Initialized!")
+    except json.JSONDecodeError as e:
+        print(f"❌ Błąd dekodowania JSON: {e}")
+else:
+    print("❌ Zmienna FIREBASE_CREDENTIALS jest pusta lub nie została poprawnie wczytana!")
